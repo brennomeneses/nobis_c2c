ba1a2a1440972e5350191f5994f0af636f536a09
brennomeneses <brennocorps@gmail.com>
2024-10-15 15:07:48 -0300

    bug fix

--- src/components/prestador/index/clube.tsx
+++ src/components/prestador/index/clube.tsx
@@ -5,7 +5,7 @@ const App: React.FC = () => (
   <>
     <center>
       <Card style={{ width: 260, textAlign: 'justify', alignContent: 'center' }}>
-        <p>Divulgue seu perfil de prestador usandos seu link de afiliado para ganhar vantagens do Clube Nobis! <a className="forumLink"> Saiba mais ></a></p>
+        <p>Divulgue seu perfil de prestador usandos seu link de afiliado para ganhar vantagens do Clube Nobis! <a className="forumLink">{" Saiba mais >"}</a></p>
       </Card>
     </center>
   </>
